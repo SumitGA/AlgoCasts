3d4c80b47b96d141cc364b87c91633ff5e46149c
Sumit Gautam <sghost33@gmail.com>
2021-06-22 23:32:14 +1000

    String reverse with array helper reduce and for loop

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,9 +7,26 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  const arr = str.split('');
-  arr.reverse();
-  return arr.join('');
+  // Solution 1
+   // const arr = str.split('');
+  // arr.reverse();
+  // return arr.join('');
+
+  //Solution 2 
+  // return str
+    // .split('')
+    // .reverse()
+    // .join('');
+
+  //solution 3
+  // let reversed = '';
+  // for(let character of str) {
+    // reversed  = character + reversed;
+  // }
+  // return reversed;
+
+  //solution 4
+  return str.split('').reduce((rev, char) => char + rev, '');
 }
 
 module.exports = reverse;
